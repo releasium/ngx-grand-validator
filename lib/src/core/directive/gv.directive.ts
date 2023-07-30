f78c348dc4104bc1cabc8eee27943975641c71e0
Konstantin <ck.kostya@gmail.com>
2023-07-30 19:22:49 +0200

    fix ts warnings

--- lib/src/core/directive/gv.directive.ts
+++ lib/src/core/directive/gv.directive.ts
@@ -6,15 +6,15 @@ import { FormMessage } from '../../components/error-message/form-msg.type';
   selector: '[GV]'
 })
 export class GVDirective implements OnInit {
-  @Input() GV: IGVModelStatic<GVModel>;
-  @Input() formMsgGroup: FormMessage;
+  @Input() GV!: IGVModelStatic<GVModel>;
+  @Input() formMsgGroup!: FormMessage;
   @Input() formGroupName: string = '';
 
   constructor(@Optional() @Host() @SkipSelf() private gvDirective: GVDirective) {}
 
   ngOnInit() {
     if (!this.GV || !this.GV.genUIMsg) {
-      throw '[GV]="???" value is required for GV directive. Add your GVModel'
+      throw '[GV]="???" value is reuqired for GV directive. Add your GVModel'
     }
     this.formMsgGroup = this.GV.genUIMsg();
     this.formMsgGroup = this.formMsgGroup || this.gvDirective.formMsgGroup[this.formGroupName];
