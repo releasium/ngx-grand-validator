712f515b4d3e56c94e759c04ceba679f8826a381
Konstantin <ck.kostya@gmail.com>
2023-08-03 00:31:57 +0200

    setup tests

--- karma.conf.js
+++ karma.conf.js
@@ -1,71 +1,44 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-const path = require("path");
 module.exports = function (config) {
   config.set({
-    customLaunchers: {
-      ChromeCustom: {
-        base: 'ChromeHeadless',
-        flags: [
-          '--no-sandbox',
-        ]
-      }
-    },
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
-      require('karma-junit-reporter'),
+      require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
-      require('karma-summary-reporter'),
-      require('karma-chrome-launcher'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
-    junitReporter: {
-      outputDir: path.join(__dirname, 'var'),
-      outputFile: 'logfile.xml',
-      useBrowserName: false,
-    },
-    coverageReporter: {
-      dir: path.join(__dirname, 'var/coverage'),
-      reporters: [
-        {type: "html"},
-        {type: "text-summary"},
-      ],
-      check: {
-        global: {
-          statements: 50,
-          branches: 35,
-          functions: 40,
-          lines: 50
-        }
-      }
-    },
     client: {
+      jasmine: {
+        // you can add configuration options for Jasmine here
+        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
+        // for example, you can disable the random execution with `random: false`
+        // or set a specific seed with `seed: 4321`
+      },
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    singleRun: true,
-    reporters: ['progress', 'kjhtml', 'junit', 'summary'],
-    summaryReporter: {
-      // 'failed', 'skipped' or 'all'
-      show: 'failed',
-      // Limit the spec label to this length
-      specLength: 50,
-      // Show an 'all' column as a summary
-      overviewColumn: true,
-      // Show a list of test clients, 'always', 'never' or 'ifneeded'
-      browserList: 'always',
-      // Use custom symbols to indicate success and failure
-      symbols: {success: '+', failure: 'x'}
+    jasmineHtmlReporter: {
+      suppressAll: true // removes the duplicated traces
     },
-    port: 9875,
-    browserNoActivityTimeout: 60000,
-    reportSlowerThan: 100,
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage/example'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' }
+      ]
+    },
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: false,
-    browsers: ['ChromeCustom']
+    browsers: ['ChromeHeadless'],
+    singleRun: true,
+    restartOnFileChange: true
   });
 };
--- test.ts
+++ test.ts
@@ -1,32 +1,15 @@
-/*
- ======= WARNING =======
- Imports must stay in a current order
- ======= WARNING =======
-*/
+// This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
-import 'zone.js/dist/zone';
 import 'zone.js/testing';
-
 import { getTestBed } from '@angular/core/testing';
-import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
-
-
-// Unfortunately there's no typing for the `__karma__` variable. Just declare it as any.
-declare const __karma__: any;
-declare const require: any;
-
-// Prevent Karma from running prematurely.
-__karma__.loaded = function () {
-};
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting
+} from '@angular/platform-browser-dynamic/testing';
 
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting()
+  platformBrowserDynamicTesting(),
+  { teardown: { destroyAfterEach: true }},
 );
-// Then we find all the tests.
-const context = require.context('./', true, /\.spec\.ts$/);
-// And load the modules.
-context.keys().map(context);
-// Finally, start Karma to run the tests.
-__karma__.start();
